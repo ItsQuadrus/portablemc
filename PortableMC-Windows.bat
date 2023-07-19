f8e056d1ba619e6eb9a74dc8b5774b657ac9a73b
ItsQuadrus <quadrus.yt@gmail.com>
2023-07-19 13:21:17 +0200

    Update PortableMC-Windows.bat

--- PortableMC-Windows.bat
+++ PortableMC-Windows.bat
@@ -10,16 +10,14 @@ echo.
 echo.
 echo.
 echo.
-echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-echo Portable Minecraft Launcher by greymesa and PiSaucer
-echo Project Website: portablemc.ml
-echo Please Check Out My Website greymesa.tech
+echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+echo Portable Minecraft Launcher by greymesa and PiSaucer / Edited by ItsQuadrus
 echo v1.4.1
-echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 if not exist "%CD%\GameData\AppData\.minecraft" mkdir "%CD%\GameData\AppData\.minecraft"
 if not exist "%CD%\GameData\Minecraft" mkdir "%CD%\GameData\Minecraft"
 if not exist "%CD%\GameData\Minecraft\Minecraft.exe" goto download
- 
+
 :launch
 color 0a
 echo Minecraft has been downloaded! It is located in GameData\Minecraft.
@@ -27,13 +25,12 @@ echo Launching...
 timeout /t 3 /nobreak > NUL
 start "Minecraft" "%CD%\GameData\Minecraft\Minecraft.exe" --workDir "%CD%\GameData\AppData\.minecraft"
 goto end
- 
+
 :download
 color 0c
 echo Minecraft launcher not found! Downloading...
 timeout /t 3 /nobreak > NUL
 powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://launcher.mojang.com/download/Minecraft.exe' '%CD%\GameData\Minecraft\Minecraft.exe'"
-powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://www.dropbox.com/s/ly1111w198fnvt0/servers.dat?dl=1' '%CD%\GameData\AppData\.minecraft\servers.dat'"
 goto launch
- 
+
 :end
